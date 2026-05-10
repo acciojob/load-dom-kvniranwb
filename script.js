4eb41a09c0cf656897c5a7c68e4c0379d6383936
kvniranwb <niranjanvt967@gmail.com>
2026-05-10 13:13:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+let c = document.getElementsByTagName("body");
+
+let h1 = document.createElement("h1");
+
+h1.textContent = "DOM load Success";
+
+c[0].appendChild(h1);
